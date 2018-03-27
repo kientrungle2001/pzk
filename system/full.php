5b548699a5098bda2e1c8c2a5eb3512f8fa55029
Le Trung Kien <kientrungle2001@gmail.com>
2018-03-27 09:24:56 +0700

    system

---
+++ system/full.php
@@ -0,0 +1,38 @@
+<Core.System id="system" bootstrap="application">
+	<Core.Loader id="loader" />
+	<Core.Request id="request" />
+	<!--
+	<core.rewrite.host name="ptnn.vn" app="nobel_ptnn" softwareId="3" />
+	<core.rewrite.host name="ptnn.vn:8080" app="nobel_ptnn" softwareId="3" />
+	<core.rewrite.host name="test1.vn" app="nobel_test" softwareId="1" siteId="1" />
+	<core.rewrite.host name="test1tq.vn" app="nobel_test" softwareId="11" />
+	<core.rewrite.host name="test1.vn:8080" app="nobel_test" softwareId="1" />
+	<core.rewrite.host name="test.vn" app="nobel_test" softwareId="2" />
+	<core.rewrite.host name="test.vn:8080" app="nobel_test" softwareId="2" />
+	<core.rewrite.host name="nobel.vn" app="nobel" softwareId="4" />
+	<core.rewrite.host name="nobel.vn:8080" app="nobel" softwareId="4" />
+	<core.rewrite.host name="cms.vn" app="nobel_cms" softwareId="5" />
+	<core.rewrite.host name="cms.vn:8080" app="nobel_cms" softwareId="5" />
+	<core.rewrite.host name="olympic.vn" app="nobel_olympic" softwareId="6" />
+	<core.rewrite.host name="olympic.vn:8080" app="nobel_olympic" softwareId="6" />
+	<core.rewrite.host name="s1.demo.nextnobels.com" app="nobel_test" softwareId="1" />
+	<core.rewrite.host name="s1.demo.nextnobels.com:8080" app="nobel_test" softwareId="1" />
+	<core.rewrite.host name="s2.demo.nextnobels.com" app="nobel_test" softwareId="2" />
+	<core.rewrite.host name="s1.demo.nextnobels.com:8080" app="nobel_test" softwareId="2" />
+	<core.rewrite.host name="demo.nextnobels.com" app="nobel" softwareId="4" />
+	<core.rewrite.host name="demo.nextnobels.com:8080" app="nobel" softwareId="4" />
+	<core.rewrite.host name="fl.360do.info" app="nobel_test" softwareId="1" />
+	<core.rewrite.host name="pmtv.vn" app="nobel_test_pmtv" softwareId="101" />
+	<core.rewrite.host name="pmtv3.vn" app="nobel_test_pmtv" softwareId="103" />
+	<core.rewrite.host name="pmtv4.vn" app="nobel_test_pmtv" softwareId="104" />
+	<core.rewrite.host name="pmtv5.vn" app="nobel_test_pmtv" softwareId="105" />
+	<core.rewrite.host name="thitai.vn" app="nobel_test" softwareId="1001" controller="Home" action="index" />
+	<core.rewrite.host name="thitai.com" app="nobel_test" softwareId="1001" controller="Home" action="index" />
+	<core.rewrite.host name="nb.thitai.vn" app="nobel_test" softwareId="1002" controller="Home" action="index" />
+	<core.rewrite.host name="tdn.thitai.vn" app="nobel_test" softwareId="9" controller="Contest" action="index" />
+	<core.rewrite.host name="nb.thitai.com" app="nobel_test" softwareId="1002" controller="Home" action="index" />
+	
+	<core.rewrite.host name="test1sn.vn" app="nobel_test" softwareId="1" siteId="2" language="true" />
+	<core.rewrite.host name="fulllooksongngu.vn" app="nobel_test" softwareId="1" siteId="2" language="true" />
+	-->
+</Core.System>
