d7c21e9d48f4d5745fcaf92ae7ed90233f3f47c5
huunv <nguyenhuu140490@gmail.com>
2018-05-18 17:10:30 +0700

    thi nang luc

---
+++ Themes/Thinangluc/layouts/education/practice/practicecontent.php
@@ -0,0 +1,292 @@
+<style>
+	.hot {
+    font-size: 10px;
+    margin: 0;
+    background: red;
+    padding: 5px 10px;
+    font-weight: bold;
+    color: #fff;
+    border-radius: 50px;
+    position: unset;
+	
+}
+.rating {
+    margin: 0 15px;
+    height: auto;
+    padding: 0;
+}
+.songuoihoc {
+    text-align: right;
+    background: url(/Themes/Thinangluc/skin/media/songuoihoc.png) no-repeat left;
+    width: 150px;
+	padding-left: 35px;
+}
+.songuoihoc span {
+    color: #fd6e23;
+}
+.baogom {
+    margin: 0;
+    padding: 0;
+	list-style: none;
+	float: left;
+	width: 100%;
+	margin-top: 15px;
+}
+.baogom li:nth-child(1):before {
+    content: "";
+    width: 20px;
+    height: 20px;
+    float: left;
+    margin-right: 5px;
+    background: url(/Themes/Thinangluc/skin/media/gift.png) no-repeat left;
+}
+.baogom li:before {
+    content: "";
+    width: 20px;
+    height: 20px;
+    float: left;
+    margin-right: 5px;
+    background-position-x: 5px;
+    background: url(/Themes/Thinangluc/skin/media/check.png) no-repeat left;
+}
+.baogom li:nth-child(1) {
+    font-weight: bold;
+}
+
+.menu-khoahoc{
+    padding: 0;
+	list-style: none;
+	float: left;
+	width: 100%;
+	margin: 15px 0px;
+	padding-bottom: 15px;
+    border-bottom: 2px solid #2e4050;
+}
+.menu-khoahoc .menu-child {
+    margin-right: 15px;
+    font-size: 16px;
+	float: left;
+}
+.menu-khoahoc .menu-child a {
+    color: #2e4050;
+    font-weight: bold;
+}
+.chiasekh{margin-top: 15px;}
+#decuong{margin-top: 15px;}
+.pl-0{padding-left: 0px;}
+.item-lienquan{float: left; width: 100%; margin-bottom: 15px;}
+</style>
+<div class="item bgcontent">
+	<div class="container">
+		<div class="item fs18 top-content bold">
+			&nbsp; &nbsp;
+			<a href="/#practice">
+			Luyện tập 
+			</a>	
+			&nbsp; &nbsp; &gt; &nbsp; &nbsp;
+			<a href="/practice/class-5/subject-Mathematics-51">
+			Toán học		</a>
+		</div>
+		
+		<div class="row">
+		
+			<div class="col-xs-12 col-md-8">
+				<iframe width="100%" height="340" src="https://www.youtube.com/embed/AzNyjWM2kt4" frameborder="0" allowfullscreen=""></iframe>
+				<div class="chiasekh">
+                    <b style="float: left; margin-top: 5px; margin-right: 15px;">Chia sẻ khóa học:</b>
+                    <a href="" target="_blank" title="Chia sẻ">
+                        <i class="fa fa-facebook-official fa-2x"></i>
+                    </a>
+                    <a href="" target="_blank" title="Chia sẻ">
+                        <i class="fa fa-google-plus-square fa-2x"></i>
+                    </a>
+                    <a href="" target="_blank" title="Chia sẻ">
+                        <i class="fa fa-twitter fa-2x"></i>
+                    </a>
+                </div>
+			</div>
+			
+			<div class="col-xs-12 col-md-4">
+				<h2 style="margin-top: 0px;">Đánh giá năng lực: Toán và Khoa học</h2>
+				
+				<div class="item">
+                    <span class="hot">HOT </span>
+        			<img class="rating" src="/Themes/Thinangluc/skin/media/rating.png" width="70px" height="15px" alt="">
+    				<span class="songuoihoc">
+    					<span>2877</span> đang học
+    				</span>
+                </div>
+				
+				<ul class="baogom">
+                    <li>Khóa Học Bao Gồm</li>
+                    <li>0 video bài giảng</li>
+                    <li>3 bài học thử</li>
+                    <li>84 bài thi trắc nghiệm</li>
+                    <li>Xem lại bài bất cứ lúc nào</li>
+                </ul>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-xs-12 col-md-8">
+				<ul class="menu-khoahoc">
+                    <li class="menu-child"><a href="#chitiet">GIỚI THIỆU CHI TIẾT</a></li>
+                    <li class="menu-child"><a href="#decuong">ĐỀ CƯƠNG</a></li>
+                </ul>
+				
+				<div class="item">
+					<div id="chitiet" class="chitiet">
+						Thông tin tuyển sinh vào 6 năm học 2018 - 2019:<br/>
+
+						Phương án kiểm tra, đánh giá năng lực: Học sinh phải thực hiện 02 bài kiểm tra (Bài tổ hợp Khoa học tự nhiên và Toán; bài tổ hợp Khoa học xã hội, tiếng Việt và tiếng Anh); Nội dung kiểm tra thuộc chương trình giáo dục tiểu học, chủ yếu nằm trong chương trình lớp 5 hiện hành của Bộ GD&ĐT; Hình thức kiểm tra trắc nghiệm khách quan kết hợp với tự luận, trong đó đảm bảo yêu cầu bốn cấp độ nhận thức: Nhận biết, thông hiểu, vận dụng cấp độ thấp và vận dụng cấp độ cao; Thời gian làm bài: 60 phút/bài kiểm tra.<br/>
+
+						Thời gian kiểm tra, đánh giá năng lực: Đợt 1 vào ngày 29/6/2018; đợt 2 vào ngày 30/6/2018.Tuyển sinh từ ngày 10/7/2018 đến hết ngày 12/7/2018.<br/>
+
+						Để giúp phụ huynh và học sinh bớt lo lắng, Hệ thống Trung tâm VINASTUDY.VN đã xây dựng khóa học "Đánh giá năng lực Khoa học Tự nhiên".<br/>
+
+						Mục đích khóa học: Là cơ hội để các em học sinh lớp 5 làm quen, thử sức với mẫu đề "kiểm tra đánh giá năng lực". Trên cơ sở đó, các em học sinh lớp 5 sẽ biết được năng lực (cả về kiến thức, kĩ năng) của bản thân đang ở mức nào? Có những lỗ hổng kiến thức cần khắc phục và có chiến lược ôn tập hợp lý, chuẩn bị cho kỳ thi vượt cấp.<br/>
+
+						Nội dung khóa học: Khóa học "Đánh giá năng lực Khoa học Tự nhiên" gồm các đề thi theo chuyên đề Khoa học Tự nhiên, Toán. Mỗi bài thi trong 60 phút, gồm các câu trắc nghiệm khách quan đảm bảo yêu cầu bốn cấp độ nhận thức: Nhận biết, thông hiểu, vận dụng cấp độ thấp và vận dụng cấp độ cao.<br/>
+
+						Kết quả:<br/>
+
+						+ Các câu hỏi sai sẽ xuất hiện nhiều ở các lần luyện tập về sau.<br/>
+
+						+ Điểm thi được chấm ngay sau khi làm và có đáp án.<br/>
+
+						+ Điểm thi được lưu vào bảng xếp hạng .<br/>
+
+						Đối tượng:<br/>
+
+						Học sinh có nhu cầu ôn thi THCS các môn Khoa học Tự nhiên- Toán.
+					</div>
+					<div id="decuong">
+						<div class="panel panel-primary">
+						  <div class="panel-heading">
+							<h3 class="panel-title">1. Bài học thử  <span class="pull-right">3 Bài học</span></h3>
+						  </div>
+						  <div class="panel-body">
+							<ul class="list-group">
+							  <li class="list-group-item">
+								<a href="#">
+								
+								4. ĐÁNH GIÁ NĂNG LỰC - TOÁN HỌC VÀ ỨNG DỤNG - SỐ TỰ NHIÊN - ĐỀ 1 
+								<span class="badge">14</span>
+								</a>
+							  </li>
+							  <li class="list-group-item">
+								<a href="#">
+								
+								4. ĐÁNH GIÁ NĂNG LỰC - TOÁN HỌC VÀ ỨNG DỤNG - SỐ TỰ NHIÊN - ĐỀ 1
+								<span class="badge">14</span>
+								</a>		
+							  </li>
+							  <li class="list-group-item">
+								<a href="#">
+								
+								4. ĐÁNH GIÁ NĂNG LỰC - TOÁN HỌC VÀ ỨNG DỤNG - SỐ TỰ NHIÊN - ĐỀ 1
+								<span class="badge">14</span>
+								</a>		
+							  </li>
+							</ul>
+						  </div>
+						</div>
+						
+						<div class="panel panel-primary">
+						  <div class="panel-heading">
+							<h3 class="panel-title">1. Bài học thử  <span class="pull-right">3 Bài học</span></h3>
+						  </div>
+						  <div class="panel-body">
+							<ul class="list-group">
+							  <li class="list-group-item">
+								<a href="#">
+								
+								4. ĐÁNH GIÁ NĂNG LỰC - TOÁN HỌC VÀ ỨNG DỤNG - SỐ TỰ NHIÊN - ĐỀ 1 
+								<span class="badge">14</span>
+								</a>
+							  </li>
+							  <li class="list-group-item">
+								<a href="#">
+								
+								4. ĐÁNH GIÁ NĂNG LỰC - TOÁN HỌC VÀ ỨNG DỤNG - SỐ TỰ NHIÊN - ĐỀ 1
+								<span class="badge">14</span>
+								</a>		
+							  </li>
+							  <li class="list-group-item">
+								<a href="#">
+								
+								4. ĐÁNH GIÁ NĂNG LỰC - TOÁN HỌC VÀ ỨNG DỤNG - SỐ TỰ NHIÊN - ĐỀ 1
+								<span class="badge">14</span>
+								</a>		
+							  </li>
+							</ul>
+						  </div>
+						</div>
+						
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-12 col-md-4">
+				<h2 class="text-center">Giảng viên</h2>
+				<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+				  <!-- Indicators -->
+				  <ol class="carousel-indicators">
+					<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+					<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+					
+				  </ol>
+
+				  <!-- Wrapper for slides -->
+				  <div class="carousel-inner" role="listbox">
+					<div class="text-center active">
+					  <img class="item img-circle" src="/Themes/Thinangluc/skin/media/gv.jpg" alt="...">
+					  <div class="item">
+						<h3>Third slide label</h3>
+						<p>
+						Nơi ở hiện tại: Đống Đa – Hà Nội Học vị: Thạc sĩ Quá trình công tác và kinh nghiệm: Thầy Nguyễn Thành Long đã có kinh nghiệm 10 năm giảng dạy với hàng trăm học sinh đạt thành tích cao trong các kì thi Violympic Toán, Thi học sinh giỏi Toán. Đặc biệt, rất nhiều học sinh của thầy thi đỗ vào các trường chuyên, chất lượng cao ở Hà Nội như: Amsterdam, Giảng Võ, Chu Văn An,.... Thầy nguyên là giáo viên Hệ thống Giáo dục Anhxtanh Hà Nội. Giáo viên Hệ thống Giáo dục Vinastudy.vn. Giáo viên đội tuyển
+						<p>
+					  </div>
+					</div>
+					
+					<div class="item">
+					  <img class="text-center img-circle" src="/Themes/Thinangluc/skin/media/gv.jpg" alt="...">
+					  <div class="item">
+					  <h3>Third slide label</h3>
+					  <p>
+						Nơi ở hiện tại: Đống Đa – Hà Nội Học vị: Thạc sĩ Quá trình công tác và kinh nghiệm: Thầy Nguyễn Thành Long đã có kinh nghiệm 10 năm giảng dạy với hàng trăm học sinh đạt thành tích cao trong các kì thi Violympic Toán, Thi học sinh giỏi Toán. Đặc biệt, rất nhiều học sinh của thầy thi đỗ vào các trường chuyên, chất lượng cao ở Hà Nội như: Amsterdam, Giảng Võ, Chu Văn An,.... Thầy nguyên là giáo viên Hệ thống Giáo dục Anhxtanh Hà Nội. Giáo viên Hệ thống Giáo dục Vinastudy.vn. Giáo viên đội tuyển
+						<p>
+					  </div>
+					</div>
+					
+				  </div>
+
+				 
+				</div>
+				
+				<h2 class="text-center">Khóa học liên quan</h2>
+				<a href="#" class="item-lienquan">
+					<div class="col-xs-12 col-md-6"> 
+						<img class="item" src="/Themes/Thinangluc/skin/media/20382.jpg" alt="" />
+					</div>
+					<div class="col-xs-12 pl-0 col-md-6">
+						<p>Ôn thi vào 6 môn Toán</p>
+						<p>Giá: 1.000.000 VNĐ</p>
+					</div>
+				</a>
+				<a href="#" class="item-lienquan">
+					<div class="col-xs-12 col-md-6"> 
+						<img class="item" src="/Themes/Thinangluc/skin/media/20382.jpg" alt="" />
+					</div>
+					<div class="col-xs-12 pl-0 col-md-6">
+						<p>Ôn thi vào 6 môn Toán</p>
+						<p>Giá: 1.000.000 VNĐ</p>
+					</div>
+				</a>
+			</div>
+		</div>
+	</div>	
+</div>
+ <img class="item mgt-60" src="/Themes/Songngu3/skin/images/bottom-content.png"/>
+ 
+ 
+ 
