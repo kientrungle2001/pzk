9925fa7f2c61138a1787bd54da359fcf6734f718
huunv90 <nguyenhuu140490@gmail.com>
2018-09-07 10:02:12 +0700

    huunv

--- Themes/Songngu3/layouts/education/test/ajaxtest.php
+++ Themes/Songngu3/layouts/education/test/ajaxtest.php
@@ -3,9 +3,9 @@
 $lang = pzk_session('language');
 $check = pzk_session('checkPayment');
 	$page = $data->get('page');
-	$class = $data->get('class'); 	
+	$class = $data->get('class'); 
+	$items = $data->getTests($class, 1410, $page, 15);	
 ?>
-{? $items = $data->getTests($class, 1410, $page, 15); ?}
 
 {each $items as $item}
 <?php $tests= $data->getTestByWeek($item['id'], 0, $check, $class); 
