0b88dee89b88e3d30ff9ee91b0ee035a74918745
Le Trung Kien <kienlt@smartosc.com>
2020-08-06 12:50:23 +0700

    remove test mode

--- index.php
+++ index.php
@@ -1,24 +1,5 @@
 <?php
 $startTime = microtime(true);
-// Cấu hình các constant
-class AssertConstant {
-	static $passed = 0;
-	static $failed = 0;
-	static $case = 1;
-}
-file_put_contents('testResult.txt', '');
-const TESTING_MODE = 1;
-function assert_equal($real, $expected) {
-	if(!TESTING_MODE) return false;
-	if($real === $expected) {
-		AssertConstant::$passed++;
-		file_put_contents('testResult.txt', 'Case '.AssertConstant::$case.' passed!' . "\r\n", FILE_APPEND);
-	} else {
-		AssertConstant::$failed++;
-		file_put_contents('testResult.txt', 'Case '.AssertConstant::$case.' failed! Expected: ' . $expected . ' but "' . $real . "\"\r\n", FILE_APPEND);
-	}
-	AssertConstant::$case++;
-}
 require_once __DIR__ . '/config.php';
 if(isset($_REQUEST['SIMPLE_MODE']) && $_REQUEST['SIMPLE_MODE']) {
 	define('SIMPLE_MODE', 1);
@@ -38,28 +19,28 @@ if(COMPILE_MODE) {
 	require_once __DIR__ . '/include.php';
 	// chạy hệ thống
 	require_once __DIR__ . '/compile/pages/system_full.php';
-	
+
 	$request = pzk_request();
 	// include các cấu hình tùy chỉnh của gói
 	if($request->getPackagePath() && is_file(BASE_DIR . '/app/'.$request->getPackagePath().'/configuration.php'))
 		require_once BASE_DIR . '/app/'.$request->getPackagePath().'/configuration.php';
-	
+
 	// include cấu hình tùy chỉnh của ứng dụng
 	if(is_file(BASE_DIR . '/app/'.$request->getAppPath().'/configuration.php'))
 		require_once BASE_DIR . '/app/'.$request->getAppPath().'/configuration.php';
-	
+
 	// include cấu hình tùy chỉnh của phần mềm
 	if(is_file(BASE_DIR . '/app/'.$request->getAppPath().'/configuration.'.$request->get('softwareId').'.php'))
 		require_once BASE_DIR . '/app/'.$request->getAppPath().'/configuration.'.$request->get('softwareId').'.php';
-	
-	
+
+
 	// chạy ứng dụng
 	$sys = pzk_element('system');
 	$application = $request->get('app');
 	require_once __DIR__ . '/compile/pages/app_'.$application.'_'.$sys->bootstrap.'.php';
-	
+
 	$app = pzk_app();
-	
+
 	// Chạy controller action
 	$controller = $request->get('controller');
 	$controller = ucfirst($controller);
@@ -69,11 +50,11 @@ if(COMPILE_MODE) {
 	$controllerObject = $app->_getController($controller);
 	if(!$controllerObject) die('No controller ' .$controller);
 	pzk_global()->set('controller', $controllerObject);
-	
+
 	// Tìm action của controller và chạy
 	$actionMethod = $action.'Action';
 	if(method_exists($controllerObject, $actionMethod)){
-		
+
 		$result = $controllerObject->$actionMethod($request->getSegment(3), $request->getSegment(4), $request->getSegment(5), $request->getSegment(6), $request->getSegment(7));
 		if($request->get('isService')) {
 			echo json_encode($result);
@@ -84,41 +65,41 @@ if(COMPILE_MODE) {
 			echo '<br />';
 			echo_memory_usage() . "\n"; // 36640
 		}
-	
+
 		$sys->halt();
 	} else {
-		
+
 		// không có action trong hệ thống
 		$sys->halt('No route ' . $action);
 	}
 } else {
 	// include các thư viện và file hệ thống
 	require_once __DIR__ . '/include.php';
-	
+
 	// Chạy hệ thống
 	$sys = pzk_parse('system/full');
-	
-	
+
+
 	// Include các cấu hình tùy chỉnh của gói
 	if(pzk_request()->getPackagePath() && is_file(BASE_DIR . '/app/'.pzk_request()->getPackagePath().'/configuration.php'))
 		require_once BASE_DIR . '/app/'.pzk_request()->getPackagePath().'/configuration.php';
-	
+
 	// Include các cấu hình tùy chỉnh của ứng dụng
 	if(is_file(BASE_DIR . '/app/'.pzk_request()->getAppPath().'/configuration.php'))
 		require_once BASE_DIR . '/app/'.pzk_request()->getAppPath().'/configuration.php';
-	
+
 	// Include các cấu hình tùy chỉnh của phần mềm
 	if(is_file(BASE_DIR . '/app/'.pzk_request()->getAppPath().'/configuration.'.pzk_request()->get('softwareId').'.php'))
 		require_once BASE_DIR . '/app/'.pzk_request()->getAppPath().'/configuration.'.pzk_request()->get('softwareId').'.php';
-	
-	
+
+
 	// Chạy ứng dụng
 	$app = $sys->getApp();
 	$app->run();
-	
+
 	$endTime = microtime(true);
 	if(isset($_REQUEST['showTime'])) echo (($endTime-$startTime) * 1000);
-	
+
 	// shutdown hệ thống
 	pzk_system()->halt();
 }
@@ -126,4 +107,3 @@ if(1) {
 	echo_memory_usage() . "\n"; // 36640
 	// echo get_server_load() . '<br />' . "\n";
 }
-
